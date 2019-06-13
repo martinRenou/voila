8599a9244f5424db3cdadb5e66e15d4747dcc1cb
martinRenou <martin.renou@gmail.com>
2019-06-13 16:07:44 +0200

    Fix tests

--- tests/app/template_cli_test.py
+++ tests/app/template_cli_test.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(__file__)
 
 @pytest.fixture
 def voila_args_extra():
-    path_test_template = os.path.abspath(os.path.join(BASE_DIR, 'test_template/share/jupyter/templates/voila/test_template/'))
+    path_test_template = os.path.abspath(os.path.join(BASE_DIR, '../test_template/share/jupyter/templates/voila/test_template/'))
     path_default = os.path.abspath(os.path.join(BASE_DIR, '../../share/jupyter/templates/voila/default'))
     return ['--template=None', '--VoilaTest.template_paths=[%r, %r]' % (path_test_template, path_default)]
 
