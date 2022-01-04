c8be887cf9b1b6eca302f905d67c8e49588e4d57
martinRenou <martin.renou@gmail.com>
2022-01-04 16:20:09 +0100

    Remove allSettled polyfill

--- packages/voila/index.js
+++ packages/voila/index.js
@@ -18,26 +18,6 @@ import { PageConfig, URLExt } from '@jupyterlab/coreutils';
 
 import { VoilaApp } from '@voila-dashboards/voila';
 
-// Promise.allSettled polyfill, until our supported browsers implement it
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
-if (Promise.allSettled === undefined) {
-  Promise.allSettled = promises =>
-    Promise.all(
-      promises.map(promise =>
-        promise.then(
-          value => ({
-            status: 'fulfilled',
-            value
-          }),
-          reason => ({
-            status: 'rejected',
-            reason
-          })
-        )
-      )
-    );
-}
-
 function loadScript(url) {
   return new Promise((resolve, reject) => {
     const newScript = document.createElement('script');
